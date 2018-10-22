6b0b8d9eb9564c90826c80d55eca446170ad85a3
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-10-22 13:40:13 +0800

    Update notifyplate.js

--- notifyplate.js
+++ notifyplate.js
@@ -3,5 +3,21 @@ document.onreadystatechange = () => {
         // Interactive
         Notify = document.getElementsByClassName("-notifyplate-")[0];
         Notify.list = Array.from(Notify.getElementsByClassName("-notifyplate-item"));
-    }
+        Notify.reformat = function () {
+            let count = 0;
+            let totalStackH = 0;
+            while (count < Notify.list.length) {
+                Notify.list[count].style.bottom = totalStackH.toString() + "px";
+                if (Notify.list[count].class != "-notifyplate-item-hidden") {
+                    totalStackH += Notify.list[count].clientHeight + 2;
+                }
+                Notify.list[count].getElementsByClassName("-notifyplate-close")[0].onclick = function () {
+                    this.parentNode.class = "-notifyplate-item-hidden";
+                    Notify.reformat();
+                };
+                count ++;
+            };
+        };
+        Notify.reformat();
+    };
 };
